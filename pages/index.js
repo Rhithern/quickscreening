3ca58ba74edd55b5f096617e20dde18efda240b1
Rhithern <rhithern@gmail.com>
2025-05-29 02:01:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import { useRef, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default function Home() {
