73df60492c5b694ec67d09fc46562a56d089128b
Rhithern <rhithern@gmail.com>
2025-07-20 15:28:44 +0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,3 +1,4 @@
+<?php
 function getCandidateNotifications($pdo, $candidateId, $limit = 10, $unreadOnly = false) {
     $sql = "SELECT * FROM notifications WHERE candidate_id = ?";
     if ($unreadOnly) {
@@ -13,4 +14,4 @@ function markNotificationRead($pdo, $notificationId, $candidateId) {
     $stmt = $pdo->prepare("UPDATE notifications SET is_read = 1 WHERE id = ? AND candidate_id = ?");
     $stmt->execute([$notificationId, $candidateId]);
 }
-
+?>
